cd5c4acb0787cccacab5cabfbd4e67bc6b75728e
NoahHerrin <noahherrin@gmail.com>
2016-04-20 18:14:39 -0700

    added basic icons for nav

---
+++ TippityTopTier/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tippity Top Tier</title>
+    <link rel="stylesheet" type="text/css"  href="style.css">
+    <link href='https://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+<h1>Register</h1>
+
+<form action="login.php" method="post">
+    <input type="text" placeholder="" name="email"><br>
+    <input type="text" placeholder="" name="username"><br>
+    <input type="password" placeholder="" name="password"><br>
+    <input type="checkbox" name="remember_login"><br>
+    <input type="submit"><br>
+</form>
+
+</body>
+</html>
