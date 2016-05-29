3e9b1fe6d9f46c20d6608bd2369cbd14b2851570
NoahHerrin <noahherrin@gmail.com>
2016-05-29 16:24:55 -0700

    GameManager Directory

---
+++ TippityTopTier/Game/GameManager.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Game Manager</title>
+</head>
+<body>
+
+    
+    <p>This page will be the dashboard for each game.
+    <ul>
+        <li>Check in with potential Disconnect Timer</li>
+        <li>Information for Connecting the two player</li>
+        <li>Confirmation that players are in same lobby</li>
+        <li>Collect Scores and submit them</li>
+        <li>Report button that sends email alerting mod to the problem</li>
+    </ul></p>
+
+    //Select Players
+    //Check in
+    //Confirm Game Settings
+    //Lobby Check in
+    //Report Game
+    //Agree Dispute Score
+    //Report to Moderator
+    
+
+</body>
+</html>
